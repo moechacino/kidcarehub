4ea4e3346dc7f1d1dd4ac291ec78090889e98104
mochacino <lanaksa28@gmail.com>
2024-05-06 21:18:06 +0700

    routes update user logout

--- src/api/routes/publicApi.ts
+++ src/api/routes/publicApi.ts
@@ -4,3 +4,4 @@ import { UserController } from "../controllers/userController";
 export const publicRouter = express.Router();
 publicRouter.route("/api/v1/user/register").post(UserController.register);
 publicRouter.route("/api/v1/user/login").post(UserController.login);
+publicRouter.route("/api/v1/user/logout").post(UserController.logout);
