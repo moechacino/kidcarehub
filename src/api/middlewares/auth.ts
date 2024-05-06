2957025b33c19569db2a2b1bc14898a6070e320e
mochacino <lanaksa28@gmail.com>
2024-05-06 20:38:10 +0700

    create auth middleware

---
+++ src/api/middlewares/auth.ts
@@ -0,0 +1,34 @@
+import jwt, { Secret, JwtPayload } from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+import { Unauthenticated } from "../errors/Unauthenticated";
+
+export interface CustomRequest extends Request {
+  user?: {
+    _id: number;
+    name: string;
+    phoneNumber: string;
+  };
+}
+
+export const authenticationMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+
+  const SECRET_KEY: Secret = process.env.JWT_SECRET || "anjay secret";
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new Unauthenticated("no token provided");
+  }
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded: any = jwt.verify(token, SECRET_KEY);
+    const { _id, name, phoneNumber } = decoded;
+    (req as CustomRequest).user = { _id, name, phoneNumber };
+    next();
+  } catch (error) {
+    throw new Unauthenticated("No access");
+  }
+};
