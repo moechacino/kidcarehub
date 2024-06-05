4ee58e7e7b4ddc9a4d4263b90b85cbc89b0fd0e3
mochacino <lanaksa28@gmail.com>
2024-06-05 20:51:06 +0700

    create writerController

--- src/api/controllers/writerController.ts
+++ src/api/controllers/writerController.ts
@@ -1,28 +1,28 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import {
-  CreateUserRequest,
-  LoginUserRequest,
-  UserResponse,
-} from "../models/userModel";
-import { UserService } from "../services/user-service";
+  CreateWriterRequest,
+  LoginWriterRequest,
+  WriterResponse,
+} from "../models/writerModel";
+import { WriterService } from "../services/writer-service";
 import { CustomRequest } from "../middlewares/auth";
 import { Unauthenticated } from "../errors/Unauthenticated";
 
-export class UserController {
+export class WriterController {
   static async register(
     request: Request,
     response: Response,
     next: NextFunction
   ) {
     try {
-      const createUserRequest: CreateUserRequest =
-        request.body as CreateUserRequest;
-      const createUserResponse: UserResponse =
-        await UserService.register(createUserRequest);
+      const writerRequest: CreateWriterRequest =
+        request.body as CreateWriterRequest;
+      const registerResponse: WriterResponse =
+        await WriterService.register(writerRequest);
 
       response.status(201).json({
         success: true,
-        data: createUserResponse,
+        data: registerResponse,
       });
     } catch (error) {
       next(error);
@@ -31,18 +31,18 @@ export class UserController {
 
   static async login(request: Request, response: Response, next: NextFunction) {
     try {
-      const loginUserRequest: LoginUserRequest =
-        request.body as LoginUserRequest;
+      const loginRequest: LoginWriterRequest =
+        request.body as LoginWriterRequest;
+      const loginResponse: WriterResponse =
+        await WriterService.login(loginRequest);
 
-      const loginUserResponse = await UserService.login(loginUserRequest);
-
-      response.cookie("token", loginUserResponse.token!, {
+      response.cookie("token", loginResponse.token!, {
         maxAge: 1000 * 60 * 60 * 2,
       });
 
       response.status(200).json({
         success: true,
-        data: loginUserResponse,
+        data: loginResponse,
       });
     } catch (error) {
       next(error);
@@ -59,11 +59,10 @@ export class UserController {
       const { _id } = request.user!;
       if (!_id) throw new Unauthenticated("You are not authorized");
 
-      const logoutUserResponse = await UserService.logout(_id);
-
+      const logoutResponse = await WriterService.logout(_id);
       response.status(200).json({
         success: true,
-        data: logoutUserResponse,
+        data: logoutResponse,
       });
     } catch (error) {
       next(error);
