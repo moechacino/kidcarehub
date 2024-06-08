aa314d281c70aac5ac5cb46835743002b9f7aca6
mochacino <lanaksa28@gmail.com>
2024-06-08 22:11:48 +0700

    index: add middleware not-found

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,41 @@
 import express, { Express, Request, Response, Application } from "express";
+import "express-async-errors";
 import dotenv from "dotenv";
 import { publicRouter } from "./api/routes/publicApi";
 import { errorHandlerMiddleware } from "./api/middlewares/errorHandler";
-
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser = require("body-parser");
+import notFoundMiddleware from "./api/middlewares/not-found";
 //For env File
 dotenv.config();
-
 export const app: Application = express();
 const port = process.env.PORT || 9000;
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(express.json());
-app.use(publicRouter);
+app.use("/api", publicRouter);
+app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 // app.get("/", (req: Request, res: Response) => {
 //   res.send("Welcome");
 // });
 
-// app.listen(port, () => {
-//   console.log(`Server is Fire at http://localhost:${port}`);
-// });
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`server is listening on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
