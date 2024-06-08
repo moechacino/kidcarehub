890da75849479fa61b4766402fa641104c4b7d05
mochacino <lanaksa28@gmail.com>
2024-06-08 22:13:34 +0700

    update:route admin

--- src/api/routes/admin/index.ts
+++ src/api/routes/admin/index.ts
@@ -2,11 +2,17 @@ import express from "express";
 import { authenticationMiddleware } from "../../middlewares/auth";
 import { verifyToken } from "../../middlewares/verifyToken";
 import { AdminController } from "../../controllers/adminController";
+import { OnlyAccessedBy } from "../../middlewares/onlyAccessedBy";
 const adminRouter = express.Router();
 
 adminRouter.route("/login").post(AdminController.login);
 adminRouter
   .route("/logout")
-  .patch(authenticationMiddleware, verifyToken, AdminController.logout);
+  .patch(
+    authenticationMiddleware,
+    verifyToken,
+    OnlyAccessedBy.admin,
+    AdminController.logout
+  );
 
 export default adminRouter;
